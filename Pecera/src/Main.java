c6786cc0ba9c3ad9b1e5e3476e4a24afb53f03af
USUARIO <USUARIO@DESKTOP-BOS45DO.cendrassos2.net>
2018-05-23 17:35:09 +0200

    Prueba

---
+++ Pecera/src/Main.java
@@ -0,0 +1,15 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+public class Main {
+	 public static void main(String[] args) {
+	        new pecera().start(args);
+	    }
+	 public static class pecera extends GraphicsProgram {
+		 @Override
+		 	public void run() {
+	            GImage fondo = new GImage("C:\\SUPERHEROI\\pelota.png");
+	            
+		
+	 	}
+	}
+}
