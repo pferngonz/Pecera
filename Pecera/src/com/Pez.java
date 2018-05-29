7ed61948fcd9ab92d5aa4adc2543ef0d8c9e5e03
Saiko315 <Saiko315@DESKTOP-7UVKFG0>
2018-05-29 19:26:25 +0200

    pecera 1.3.1

--- Pecera/src/com/Pez.java
+++ Pecera/src/com/Pez.java
@@ -3,13 +3,36 @@ package com;
 import acm.graphics.GImage;
 
 public class Pez extends animalmarino{
+	private int X=0;
+	private int Y=0;
+	public void setX(int posiciones) {
+		this.X=posiciones;
+	}
+	public void setY(int posiciones) {
+		this.Y=posiciones;
+	}
+	public int getX() {
+		return X;
+	}
+	public int getY() {
+		return Y;
+	}
 	public void reproducirse() {
 		
 	}
 	public void comer(){
 		
 	}
-	public void mover() {
+	public void mover(GImage fondo, GImage imagen) {
+		if(X != fondo.getHeight()) {
+			this.X=this.X+1;
+			System.out.println(X);
+			imagen.move(5, 0);
+		}
 		
+	}
+	public void posicion(int [][] posicion, int i) {
+		this.X=posicion[0][i];
+		this.Y=posicion[0][i];
 	}
 }
